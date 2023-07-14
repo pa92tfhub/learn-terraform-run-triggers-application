89eaa2ba3384b4c14c5e06e4ed300537acac6249
pa92tfhub <139500419+pa92tfhub@users.noreply.github.com>
2023-07-14 11:55:10 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@
 variable "tfc_org_name" {
   description = "Name of the Terraform Cloud Organization"
   type        = string
-  default     = "hashicorp-learn"
+  default     = "MAIFterraformtest"
 }
 
 variable "tfc_network_workspace_name" {
